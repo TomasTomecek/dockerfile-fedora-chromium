db096e78710ccb997d023006abcab5c64c5bce36
Tomas Tomecek <ttomecek@redhat.com>
2016-03-02 12:34:04 +0100

    quiet build

--- install_chromium.sh
+++ install_chromium.sh
@@ -8,14 +8,15 @@ install_chromium_from_copr() {
 
 install_chromium_from_fp() {
     printf "[spot-chromium]\nname=spot-chromium\nbaseurl=https://repos.fedorapeople.org/repos/spot/chromium/fedora-21/x86_64/\nenabled=1\ngpgcheck=0" >/etc/yum.repos.d/spot-chromium.repo
-    dnf install -y chromium
+    dnf install -y chromium >/dev/null
 }
 
-install_chromium_from_copr || install_chromium_from_fp || exit 2
+# install_chromium_from_copr || install_chromium_from_fp || exit 2
+install_chromium_from_copr || exit 2
 
 # FIXME: minimize only needed deps
-dnf install -y libv4l pango libexif-devel pangox-compat
+dnf install -y libv4l pango libexif-devel pangox-compat >/dev/null
 # chromium uses these fonts
-dnf install -y dejavu\*fonts\*
+dnf install -y dejavu\*fonts\* >/dev/null
 
 dnf clean all
